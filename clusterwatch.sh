9b9810fabce33f8dbceb7bd6aa0ca1de1cd14644
Joakim Karlsson <jk@patientsky.com>
2017-01-30 20:47:51 +0100

    buggfixes & improvements

--- clusterwatch.sh
+++ clusterwatch.sh
@@ -8,20 +8,24 @@ if [ -z "$CLUSTER_NAME" ]; then
 fi
 
 # Get env variables
+ADMIN_PORT=${ADMIN_PORT:-6032}
+ADMIN_USER=${ADMIN_USER:-admin}
+ADMIN_PASS=${ADMIN_PASS:-admin}
 CLUSTER_NAME=${CLUSTER_NAME:-cluster01}
+DISCOVERY_SLEEP=${DISCOVERY_SLEEP:-15}
 ###
 
 function mycnf_gen() {
-    printf "[client]\nuser = %s\npassword = %s" "${ADMIN_USER:-admin}" "${ADMIN_PASS:-admin}"
+    printf "[client]\nuser = %s\npassword = %s\nport = %s\nhost = %s" "${ADMIN_USER}" "${ADMIN_PASS}" "${ADMIN_PORT}" "127.0.0.1"
 }
 
-function update_proxysql() {
-	mysql --defaults-file=<(mycnf_gen) -h 127.0.0.1 -P${ADMIN_PORT:-6032} -e "LOAD MYSQL SERVERS TO RUNTIME; SAVE MYSQL SERVERS TO DISK;"
+function mycnf_rootgen() {
+    printf "[client]\nuser = %s\npassword = %s" "root" "${MYSQL_ROOT_PASSWORD}"
 }
 
 function update_serverlist() {
 	# Get current servers in proxysql
-	IPS=`mysql --defaults-file=<(mycnf_gen) -h 127.0.0.1 -P${ADMIN_PORT:-6032} -B --disable-column-names -e 'SELECT hostname from mysql_servers' | sort`
+	IPS=`mysql --defaults-file=<(mycnf_gen) -B --disable-column-names -e 'SELECT hostname from mysql_servers' | sort`
 	
 	# Get cluster cluster members from discovery
 	NEWIPS=`curl -s http://$DISCOVERY_SERVICE/v2/keys/pxc-cluster/$CLUSTER_NAME/ | jq -r '.node.nodes[]?.key' | awk -F'/' '{print $(NF)}' | sort`
@@ -35,56 +39,63 @@ function update_serverlist() {
 	
 	SDIFF=`diff --old-line-format='-%L' --new-line-format='+%L' --unchanged-line-format='=%L' <(for IP in $IPS; do echo $IP; done) <(for IP in $NEWIPS; do echo $IP; done)`
 	
+	CHANGES=false
 	for SERVER in $SDIFF
 	do
 		if [[ $SERVER == -* ]]
 		then
 			echo "$(date +'%Y-%m-%d %H:%M:%S,%3N') ${ME} REMOVE ${SERVER:1}"
-			mysql --defaults-file=<(mycnf_gen) -h 127.0.0.1 -P${ADMIN_PORT:-6032} -e "DELETE FROM mysql_servers where hostname='${SERVER:1}';"
+			mysql --defaults-file=<(mycnf_gen) -e "DELETE FROM mysql_servers where hostname='${SERVER:1}';"
+			CHANGES=true
 		elif [[ $SERVER == +* ]]
 		then 
 			echo "$(date +'%Y-%m-%d %H:%M:%S,%3N') ${ME} ADD ${SERVER:1}"
-			mysql --defaults-file=<(mycnf_gen) -h 127.0.0.1 -P${ADMIN_PORT:-6032} -e "INSERT INTO mysql_servers (hostgroup_id, hostname, port, max_replication_lag) VALUES (0, '${SERVER:1}', 3306, 20);"
+			mysql --defaults-file=<(mycnf_gen) -e "INSERT INTO mysql_servers (hostgroup_id, hostname, port, max_replication_lag) VALUES (0, '${SERVER:1}', 3306, 20);"
+			CHANGES=true
 		fi
 	done
+	if [[ ${CHANGES} == true ]]
+	then
+		mysql --defaults-file=<(mycnf_gen) -h 127.0.0.1 -P${ADMIN_PORT} -e "LOAD MYSQL SERVERS TO RUNTIME; SAVE MYSQL SERVERS TO DISK;"
+	fi
 }
 
 function setup_monitoring() {
     # Best practice is to have a not privileged user here with only grant usage.
-	mysql -h 127.0.0.1 -P${ADMIN_PORT} -u${ADMIN_USER} -p${ADMIN_PASS} -e "UPDATE global_variables SET variable_value='${ADMIN_USER}' WHERE variable_name='mysql-monitor_username';"
-	mysql -h 127.0.0.1 -P${ADMIN_PORT} -u${ADMIN_USER} -p${ADMIN_PASS} -e "UPDATE global_variables SET variable_value='${ADMIN_PASS}' WHERE variable_name='mysql-monitor_password';"
-	mysql -h 127.0.0.1 -P${ADMIN_PORT} -u${ADMIN_USER} -p${ADMIN_PASS} -e "LOAD MYSQL VARIABLES TO RUNTIME; SAVE MYSQL VARIABLES TO DISK;"
+    sleep 15 # wait for proxysql to start gracefully
+    echo "$(date +'%Y-%m-%d %H:%M:%S,%3N') ${ME} Setup monitor user in proxysql"
+	mysql --defaults-file=<(mycnf_gen) -e "UPDATE global_variables SET variable_value='${ADMIN_USER}' WHERE variable_name='mysql-monitor_username';"
+	mysql --defaults-file=<(mycnf_gen) -e "UPDATE global_variables SET variable_value='${ADMIN_PASS}' WHERE variable_name='mysql-monitor_password';"
+	mysql --defaults-file=<(mycnf_gen) -e "LOAD MYSQL VARIABLES TO RUNTIME; SAVE MYSQL VARIABLES TO DISK;"
 }
 
 
 echo "$(date +'%Y-%m-%d %H:%M:%S,%3N') ${ME} START"
 
-if [ "${MYSQL_BOOTSTRAP}" == "true" ]; then
+if [[ ${MYSQL_BOOTSTRAP} == true ]]; then
 	echo "$(date +'%Y-%m-%d %H:%M:%S,%3N') ${ME} Creating mysql superuser"
 	
 	sleep 10 # let proxysql start gracefully before continuing
 
-	for i in $(curl http://$DISCOVERY_SERVICE/v2/keys/pxc-cluster/$CLUSTER_NAME/ | jq -r '.node.nodes[]?.key' | awk -F'/' '{print $(NF)}')
+	for SERVER in $(curl -s http://$DISCOVERY_SERVICE/v2/keys/pxc-cluster/$CLUSTER_NAME/ | jq -r '.node.nodes[]?.key' | awk -F'/' '{print $(NF)}')
 	do
-	        mysql -h $i -uroot -p${MYSQL_ROOT_PASSWORD} -e "GRANT ALL ON *.* TO '${MYSQL_PROXY_USER}'@'%' IDENTIFIED BY '${MYSQL_PROXY_PASS}'"
-	        mysql -h $i -uroot -p${MYSQL_ROOT_PASSWORD} -e "GRANT GRANT OPTION ON *.* TO '${MYSQL_PROXY_USER}'@'%';"
+	        mysql --defaults-file=<(mycnf_rootgen) -h${SERVER} -e "GRANT ALL ON *.* TO '${MYSQL_PROXY_USER}'@'%' IDENTIFIED BY '${MYSQL_PROXY_PASS}'"
+	        mysql --defaults-file=<(mycnf_rootgen) -h${SERVER} -e "GRANT GRANT OPTION ON *.* TO '${MYSQL_PROXY_USER}'@'%';"
 	 done
 	
-	mysql -h 127.0.0.1 -P${ADMIN_PORT} -u${ADMIN_USER} -p${ADMIN_PASS} -e "INSERT INTO mysql_users (username, password, active, default_hostgroup, max_connections) VALUES ('$MYSQL_PROXY_USER', '$MYSQL_PROXY_PASS', 1, 0, 200);"
-	mysql -h 127.0.0.1 -P${ADMIN_PORT} -u${ADMIN_USER} -p${ADMIN_PASS} -e "LOAD MYSQL USERS TO RUNTIME; SAVE MYSQL USERS TO DISK;"
+	mysql --defaults-file=<(mycnf_gen) -e "INSERT INTO mysql_users (username, password, active, default_hostgroup, max_connections) VALUES ('${MYSQL_PROXY_USER}', '${MYSQL_PROXY_PASS}', 1, 0, 200);"
+	mysql --defaults-file=<(mycnf_gen) -e "LOAD MYSQL USERS TO RUNTIME; SAVE MYSQL USERS TO DISK;"
 
 fi
 
 setup_monitoring
 
 while true; do
-	sleep ${DISCOVERY_SLEEP:-15}
+	echo "$(date +'%Y-%m-%d %H:%M:%S,%3N') ${ME} PiNG"
+	sleep ${DISCOVERY_SLEEP}
 
 	# Update the serverlist
 	update_serverlist
-	
-	# Update users & servers in runtime
-	update_proxysql
 	
 done
 
--- start_node.sh
+++ start_node.sh
@@ -1,14 +1,13 @@
 CLUSTER_NAME=${CLUSTER_NAME:-cluster01}
 ETCD_HOST=${ETCD_HOST:-10.50.41.11:4001}
 
-echo "Starting new ProxySQL on $NETWORK_NAME ..."
-docker run -d -p 3306:3306 -p 6032:6032 --name=${CLUSTER_NAME}_proxysql \
+docker run --rm --name=${CLUSTER_NAME}_proxysql \
+	-p 3306:3306 -p 6032:6032 \
 	-e CLUSTER_NAME=$CLUSTER_NAME \
-	-e MYSQL_BOOTSTRAP="true"
+	-e MYSQL_BOOTSTRAP=false \
 	-e DISCOVERY_SERVICE=$ETCD_HOST \
 	-e MYSQL_ROOT_PASSWORD="str0nkpassword!" \
 	-e MYSQL_PROXY_USER="admin" \
 	-e MYSQL_PROXY_PASS="s3cret" \
 	proxysql
-echo "Started $(docker ps -l -q)"
 
